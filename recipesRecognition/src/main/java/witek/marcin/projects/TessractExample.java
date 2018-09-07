b2acc3c01e2493664c1bd1eea0365404c5a31d6a
dckx37 <dckx37@motorolasolutions.com>
2018-09-07 15:24:44 +0200

    dodanie jezyka polskiego finally works

--- recipesRecognition/src/main/java/witek/marcin/projects/TessractExample.java
+++ recipesRecognition/src/main/java/witek/marcin/projects/TessractExample.java
@@ -3,22 +3,46 @@ package witek.marcin.projects;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
 
 import java.io.File;
 
 public class TessractExample {
-
-    public static void main(String[] args) {
-        File imageFile = new File("C:\\prywatne\\expensesControl\\recipesRecognition\\src\\main\\resources\\ocr-test.jpg");
+    
+    private void testEng(){
+        String pathToJpg = "C:\\prywatne\\expensesControl\\recipesRecognition\\src\\main\\resources\\ocr-test.jpg";
+        File imageFile = new File(pathToJpg);
         ITesseract instance = new Tesseract();
-
+        String tessDataFolder = LoadLibs.extractTessResources("tessdata").getAbsolutePath();
+        instance.setDatapath(tessDataFolder);
         try {
             String string = instance.doOCR(imageFile);
             System.out.println(string);
         } catch (TesseractException e) {
             e.printStackTrace();
         }
-
     }
     
+    private void testPolish(){
+        String pathToJpg = "C:\\prywatne\\expensesControl\\recipesRecognition\\src\\main\\resources\\ocr-test-pl.jpg";
+        File imageFile = new File(pathToJpg);
+        ITesseract instance = new Tesseract();
+        String tessDataFolder = LoadLibs.extractTessResources("tessdata").getAbsolutePath();
+        instance.setLanguage("pol");
+        instance.setDatapath(imageFile.getParent());
+        try {
+            String string = instance.doOCR(imageFile);
+            System.out.println(string);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        TessractExample tessractExample = new TessractExample();
+        tessractExample.testEng();
+        tessractExample.testPolish();
+
+    }
+
 }
